8bd629c000d03b6972275bb5943ee784e03f4a4a
Madhuri-Giri <madhurigoswami23475@gmail.com>
2024-05-04 19:27:35 +0530

    Navbar completed

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <section id='home'>
+      <div className='banner_image'></div>
+        <div className='container'>
+            <div className='banner_outer'>
+                <h3 className='title'>WE PROMOT YOUR <span>BUSINESS</span></h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti recusandae, laborum delectus enim itaque veniam. Itaque error ipsum asperiores numquam!</p>
+                <div className='btn_wrapper'>
+                    <a className='btn' href='/'> GET STARTED</a>
+                </div>
+        </div>
+      </div>
+
+      </section>
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react'
+import  "../style/Navbar.css";
+import logo from "../assets/logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-scroll';
+export default function Navbar() {
+
+    const[sidenav, setSidenav] = useState(false);
+
+    const[sticky, setSticky] = useState(false);
+
+    const menuIcon = <FontAwesomeIcon icon={faBars} />
+
+    const sidenavShow = ()=>{
+        setSidenav(!sidenav);
+    }
+
+    useEffect(()=>{
+        const handleScroll = () =>{
+            setSticky(window.scrollY > 20);
+        }
+        window.addEventListener('scroll', handleScroll);
+        return() => window.removeEventListener('scroll', handleScroll);
+    })
+
+  return (
+    <header id='site_header' className={`${sticky ? 'sticky':''}`}>
+        <div className='container'>
+            <nav className='navbar' id='Navbar'>
+                <div className='navbar_brand'>
+                    <a href='/'>
+                        <img src={logo} alt="" />
+                    </a>
+                </div>
+                <div className='navbar_toggler' onClick={sidenavShow}>
+                {menuIcon}
+                </div>
+                <div className={`menu_items ${sidenav===true? ' active':''}`}>
+                    <ul>
+                        <li>
+                            <Link activeClass='active' to='home' spy="true" smooth="true">Home</Link>
+                        </li>
+                        <li>
+                            <Link  to='about' spy="true" smooth="true">About Us</Link>
+                        </li>
+                        <li>
+                            <Link  to='service' spy="true" smooth="true">Services</Link>
+                        </li>
+                         <li>
+                            <Link  to='blog' spy="true" smooth="true">Blog</Link>
+                        </li>
+                        <li>
+                            <Link  to='contact' spy="true" smooth="true">Contact Us</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+  )
+}
